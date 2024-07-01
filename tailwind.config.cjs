96b9112f9bd30c4906c83d2faef034ad95ed0c50
NySominea <sominea.ny77@gmail.com>
2024-07-01 13:48:01 +0700

    feat: :package: config color and font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,13 +4,19 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#ed333a",
+        secondary: "#132241",
+      },
       fontFamily: {
-        sans: [
-          "Bricolage Grotesque Variable",
-          "Inter Variable",
-          "Inter",
-          ...defaultTheme.fontFamily.sans,
-        ],
+        sans: ["Hanuman", ...defaultTheme.fontFamily.sans],
+        koulen: ["Koulen", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        exhibition: "url(/src/assets/backgrounds/bg_exhibition.png)",
+        "exhibition-sm": "url(/src/assets/backgrounds/bg_exhibition-sm.png)",
+        schedule: "url(/src/assets/backgrounds/bg_schedule.png)",
+        "schedule-sm": "url(/src/assets/backgrounds/bg_schedule-sm.png)",
       },
     },
   },
