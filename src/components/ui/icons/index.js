c2115a4251cdd551a92b0b50a6a9f1f256fb2dc6
NySominea <sominea.ny77@gmail.com>
2024-07-01 13:51:03 +0700

    feat: :sparkles: add footer

--- src/components/ui/icons/index.js
+++ src/components/ui/icons/index.js
@@ -1,1 +1,8 @@
+export { default as Date } from "./date.astro";
+export { default as Facebook } from "./facebook.astro";
+export { default as Instagram } from "./instagram.astro";
+export { default as LinkedIn } from "./linkedin.astro";
+export { default as Map } from "./map.astro";
 export { default as Tick } from "./tick.astro";
+export { default as Time } from "./time.astro";
+export { default as Web } from "./web.astro";
