ccd522d7fe3b87870ba062ad0f1601ec56063a91
NySominea <sominea.ny77@gmail.com>
2024-07-02 11:54:02 +0700

    feat: :globe_with_meridians: setup localization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+  km: "Khmer",
+  en: "English",
+};
+
+export const defaultLang = "km";
+export const showDefaultLang = false;
+
+export const ui = {
+  km: {
+    "headline.title":
+      "The Australian School <br> <span class='text-secondary'>Information day 2024</span>",
+    "exhibition.title":
+      "The largest Australian Early Study Abroad Fair in 2024",
+    "exhibition.description":
+      "We invite you to a meeting with prestigious Australian private schools! <br><br> Meet in person with admission officials from prestigious Australian schools in Cambodia",
+    "exhibition.date": "Sunday 8th September 2024",
+    "exhibition.time": "1:00pm - 5:00pm",
+    "exhibition.location": "Hyatt Regency Hotel, Phnom Penh",
+
+    "button.register": "Register",
+    "button.brochure": "Exhibition Brochure",
+  },
+  en: {},
+} as const;
