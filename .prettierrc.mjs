3daa4a8bc270d9b89b164581ce20b9230394932c
NySominea <sominea.ny77@gmail.com>
2024-07-02 13:03:50 +0700

    fix(style): lint

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,5 +1,16 @@
 /** @type {import("prettier").Config} */
 export default {
+  // printWidth: 120,
+  // semi: true,
+  // singleQuote: true,
+  // tabWidth: 2,
+  // trailingComma: "es5",
+  // useTabs: false,
+
+  // plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+
+  // overrides: [{ files: "*.astro", options: { parser: "astro" } }],
+
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [
     {
--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,21 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import typescriptParser from "@typescript-eslint/parser";
+
 export default [
-  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["flat/recommended"],
   {
-    rules: {},
+    files: ["**/*.{js,jsx,astro}"],
+    rules: {
+      "no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+    },
+  },
+  {
+    ignores: [
+      "dist",
+      "node_modules",
+      ".github",
+      "types.generated.d.ts",
+      ".astro",
+    ],
   },
 ];
