9945c5a48aba57b14bccf945b27b976b7b2a5a7c
NySominea <sominea.ny77@gmail.com>
2024-07-05 21:58:35 +0700

    chore: remove unused files

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -32,21 +32,25 @@ export const ui = {
       "An environment to grow among teachers, fellow students, and surrounding community.",
     "whyChooseUs.reason.approach":
       "A holistic approach to education in all aspects of daily and academic life.",
-    "whyChooseUs.reason.program":
-      "Schools with different programs and fee levels.",
+    "whyChooseUs.reason.prestigious":
+      "Prestigious non-government schools and world-class facilities",
     "whyChooseUs.reason.school":
       "With over 400 quality Australian schools available with different programs and affordable fee levels.",
 
     "whyAttend.title":
       "Why should you attend the Australian Schools Information Day?",
     "whyAttend.description":
-      "The decision to come to Australia to study is exciting, but choosing the right schools is tough! Visit AEAS AUSTRALIAN SCHOOLS INFORMATION DAY and have a 1-on-1 consultation with interpreters available at every booth. This is a great opportunity to gather information and attend the free parent seminars. <br><br>Don't miss this chance to take the first step towards your Australian education adventure! Admission is free, join the AUSTRALIAN SCHOOLS INFORMATION DAY 2024 and start exploring your options today!",
+      "The decision to come to Australia to study is exciting, but choosing the right schools is tough! Visit AEAS AUSTRALIAN SCHOOLS INFORMATION DAY and have a 1-on-1 consultation with interpreters available at every booth. This is a great opportunity to gather information and attend the free parent seminars.<br><br>" +
+      "There are  SPECIAL SCHOLARSHIPS AND ENROLMENT INCENTIVES AVAILABLE TO FAMILIES THAT ATTEND THE EXHIBITION.<br><br>" +
+      "Don't miss this chance to take the first step towards your Australian education adventure! Admission is free, join the AUSTRALIAN SCHOOLS INFORMATION DAY 2024 and start exploring your options today!",
 
-    "school.title": "Schools Participants",
+    "school.title": "MEET THESE TOP AUSTRALIAN SCHOOLS",
+    "school.description": "Click on the school logo to a view a full profile",
 
-    "aboutUs.title": "About AEAS",
+    "aboutUs.title": "How can AEAS help you on you early study abroad journey?",
     "aboutUs.description":
-      "AEAS provides English language assessment developed specifically for international primary and secondary students wishing to study in high school abroad. The AEAS test assesses English proficiency, mathematical reasoning and general ability skills and provides parents, students and agents with a comprehensive understanding of their current abilities and a recommendation for the year level of entry in an Australian classroom. The report also highlights any areas of English language they may need support with.",
+      "AEAS provides English language assessment developed specifically for international primary and secondary students wishing to study in high school abroad. The AEAS test assesses English proficiency, mathematical reasoning and general ability skills and provides parents, students and agents with a comprehensive understanding of their current abilities and a recommendation for the year level of entry in an Australian classroom. The report also highlights any areas of English language they may need support with.<br><br>" +
+      "AEAS has also organised Australian Schools exhibitions in countries around the world for over 30 years. In this time, we’ve helped thousands of families and students connect with top quality Australian schools, and AEAS events have often been the launchpad for successful study abroad pathways. AEAS is the expert on all things early study abroad!",
 
     "footer.title": "Connect with Us",
 
@@ -79,21 +83,26 @@ export const ui = {
       "បរិយាកាសដើម្បីរីកចម្រើនសម្រាប់គ្រូ សិស្ស និងសហគមន៍ជុំវិញ",
     "whyChooseUs.reason.approach":
       "វិធីសាស្រ្តរួមចំពោះការអប់រំក្នុងគ្រប់ទិដ្ឋភាពនៃជីវិតប្រចាំថ្ងៃ និងការសិក្សា",
-    "whyChooseUs.reason.program":
-      "សាលារៀនដែលមានកម្មវិធី និងកម្រិតថ្លៃសិក្សាខុសៗគ្នា",
+    "whyChooseUs.reason.prestigious":
+      "សាលាក្រៅរដ្ឋាភិបាលដ៏មានកិត្យានុភាព និងសម្ភារៈបរិក្ខារលំដាប់ពិភពលោក",
     "whyChooseUs.reason.school":
       "ជាមួយនឹងសាលារៀនដែលមានគុណភាពជាង ៤០០ សាលា កម្មវិធីសិក្សាផ្សេងៗគ្នា និងកម្រិតថ្លៃសិក្សាសមរម្យ ",
 
     "whyAttend.title":
       "ហេតុអ្វីបានជាអ្នកគួរចូលរួមក្នុងថ្ងៃពិព័រណ៍ព័ត៌មានសាលានៅប្រទេសអូស្រ្តាលី?",
     "whyAttend.description":
-      "ការសម្រេចចិត្តឱ្យកូនមករៀននៅអូស្ត្រាលីគឺពិតជារភើប ប៉ុន្តែការជ្រើសរើសសាលាត្រឹមត្រូវគឺពិបាកណាស់! សូមអញ្ញើញចូលរួម AEAS ពិព័រណ៍ព័ត៌មានសាលានៅប្រទេសអូស្រ្តាលី ដែលនឹងមានអ្នកពិគ្រោះយោបល់ ១ ទល់នឹង ១ ថែមទាំងមានអ្នកបកប្រែសម្រាប់លោកអ្នកនៅគ្រប់ស្តង់ពិព័រណ៍។ នេះគឺជាឱកាសដ៏ល្អក្នុងការមកទទួលយកព័ត៌មាន និងចូលរួមសិក្ខាសាលាមាតាបិតាដោយឥតគិតថ្លៃ។ <br><br>កុំឱ្យឱកាសដ៏ល្អនេះកន្លងផុតទៅ សូមអញ្ញើញចូលរួមដោយសេរី ពិព័រណ៍ព័ត៌មានសាលានៅប្រទេសអូស្រ្តាលី ឆ្នាំ២០២៤ ហើយចាប់ផ្តើមធ្វើការសិក្សាស្វែងយល់ពីជម្រើសសម្រាប់កូនៗរបស់អ្នកនៅថ្ងៃនេះ!",
+      "ការសម្រេចចិត្តឱ្យកូនមករៀននៅអូស្ត្រាលីគឺពិតជារភើប ប៉ុន្តែការជ្រើសរើសសាលាត្រឹមត្រូវគឺពិបាកណាស់! សូមអញ្ញើញចូលរួម AEAS ពិព័រណ៍ព័ត៌មានសាលានៅប្រទេសអូស្រ្តាលី ដែលនឹងមានអ្នកពិគ្រោះយោបល់ ១ ទល់នឹង ១ ថែមទាំងមានអ្នកបកប្រែសម្រាប់លោកអ្នកនៅគ្រប់ស្តង់ពិព័រណ៍។ នេះគឺជាឱកាសដ៏ល្អក្នុងការមកទទួលយកព័ត៌មាន និងចូលរួមសិក្ខាសាលាមាតាបិតាដោយឥតគិតថ្លៃ។ <br><br>" +
+      "មានអាហារូបករណ៍ពិសេស និងការលើកទឹកចិត្តសម្រាប់ការចុះឈ្មោះដែលអាចរកបានសម្រាប់ក្រុមគ្រួសារដែលចូលរួមក្នុងការតាំងពិព័រណ៍នេះ។<br><br>" +
+      "កុំឱ្យឱកាសដ៏ល្អនេះកន្លងផុតទៅ សូមអញ្ញើញចូលរួមដោយសេរី ពិព័រណ៍ព័ត៌មានសាលានៅប្រទេសអូស្រ្តាលី ឆ្នាំ២០២៤ ហើយចាប់ផ្តើមធ្វើការសិក្សាស្វែងយល់ពីជម្រើសសម្រាប់កូនៗរបស់អ្នកនៅថ្ងៃនេះ!",
 
-    "school.title": "សាលាដៃគូសហការណ៍",
+    "school.title": "ជួបជាមួយសាលាអូស្ត្រាលីកំពូលៗ",
+    "school.description": "សុំចុចលើរូប ដើម្បីមើលព័ត៌មានលំអិតរបស់សាលានីមួយៗ",
 
-    "aboutUs.title": "តើ AEAS គឺជាអ្វី?",
+    "aboutUs.title":
+      "តើ AEAS អាច​ជួយ​លោកអ្នក​ដូចម្ដេច​ក្នុង​ការ​ធ្វើ​ដំណើរ​ទៅ​សិក្សា​នៅ​បរទេស?",
     "aboutUs.description":
-      "AEAS ផ្តល់ការធ្វើតេស្តភាសាអង់គ្លេសដែលត្រូវបានបង្កើតឡើងជាពិសេសសម្រាប់សិស្សកម្រិតបឋមសិក្សា និងមធ្យមសិក្សា ដែលមានបំណងទៅសិក្សានៅវិទ្យាល័យនៅប្រទេសអូស្ត្រាលី។ ការធ្វើតេស្ត AEAS វាយតម្លៃជំនាញភាសាអង់គ្លេស សមត្ថភាពហេតុផលគណិតវិទ្យា និងជំនាញសមត្ថភាពទូទៅ ហើយផ្តល់ឱ្យឪពុកម្តាយ សិស្ស និងភ្នាក់ងារនាៗ នូវការយល់ដឹងដ៏ទូលំទូលាយអំពីសមត្ថភាព របស់ពួកគេ ក៏ដូចជាការណែនាំសម្រាប់កម្រិតឆ្នាំ/ថ្នាក់នៃការចូលរៀនតាមស្តង់ដារនៅប្រទេសអូស្ត្រាលី។ លទ្ធផលតេស្តក៏បានគូសបញ្ជាក់ពីផ្នែកណាមួយនៃភាសាអង់គ្លេសដែលពួកគេអាចត្រូវការការពង្រឹងបន្ថែមផងដែរ។",
+      "AEAS ផ្តល់ការធ្វើតេស្តភាសាអង់គ្លេសដែលត្រូវបានបង្កើតឡើងជាពិសេសសម្រាប់សិស្សកម្រិតបឋមសិក្សា និងមធ្យមសិក្សា ដែលមានបំណងទៅសិក្សានៅវិទ្យាល័យនៅប្រទេសអូស្ត្រាលី។ ការធ្វើតេស្ត AEAS វាយតម្លៃជំនាញភាសាអង់គ្លេស សមត្ថភាពហេតុផលគណិតវិទ្យា និងជំនាញសមត្ថភាពទូទៅ ហើយផ្តល់ឱ្យឪពុកម្តាយ សិស្ស និងភ្នាក់ងារនាៗ នូវការយល់ដឹងដ៏ទូលំទូលាយអំពីសមត្ថភាព របស់ពួកគេ ក៏ដូចជាការណែនាំសម្រាប់កម្រិតឆ្នាំ/ថ្នាក់នៃការចូលរៀនតាមស្តង់ដារនៅប្រទេសអូស្ត្រាលី។ លទ្ធផលតេស្តក៏បានគូសបញ្ជាក់ពីផ្នែកណាមួយនៃភាសាអង់គ្លេសដែលពួកគេអាចត្រូវការការពង្រឹងបន្ថែមផងដែរ។<br><br>" +
+      "AEAS ក៏​បាន​រៀបចំ​ការ​តាំង​ពិព័រណ៍​សាលា​អូស្ត្រាលី​ក្នុង​ប្រទេស​ជុំវិញ​ពិភពលោក​អស់​រយៈពេល​ជាង 30 ឆ្នាំ​មក​ហើយ។ នៅពេលនេះ យើងបានជួយគ្រួសារ និងសិស្សរាប់ពាន់នាក់ឱ្យភ្ជាប់ទំនាក់ទំនងជាមួយសាលាអូស្ត្រាលីដែលមានគុណភាពខ្ពស់ ហើយព្រឹត្តិការណ៍ AEAS តែងតែជាកម្មវិធីចាប់ផ្តើមសម្រាប់ផ្លូវជោគជ័យក្នុងការសិក្សានៅបរទេស។ AEAS ជាអ្នកជំនាញលើគ្រប់រឿងទាំងអស់ សិក្សានៅបរទេសដំបូង!",
 
     "footer.title": "ភ្ជាប់ទំនាក់ទំនងជាមួយយើង",
 
