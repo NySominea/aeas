df037a1e621f1ab315127ccfc2616615cd1d126d
NySominea <sominea.ny77@gmail.com>
2024-07-07 22:39:40 +0700

    fix: on switch language

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -16,6 +16,8 @@ export function useTranslations(lang: keyof typeof ui) {
 
 export function useTranslatedPath(lang: keyof typeof ui) {
   return function translatePath(path: string, l: string = lang) {
-    return !showDefaultLang && l === defaultLang ? path : `/aeas/${l}${path}`;
+    return !showDefaultLang && l === defaultLang
+      ? `/aeas${path}`
+      : `/aeas/${l}${path}`;
   };
 }
